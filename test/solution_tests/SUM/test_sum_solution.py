55e727249eef6b790da11c12afe900aefb3f2341
sbx_user1051 <sbx_user1051@169.254.4.197>
2025-04-25 13:07:29 +0000

    Fri Apr 25 13:07:29 UTC 2025

--- test/solution_tests/SUM/test_sum_solution.py
+++ test/solution_tests/SUM/test_sum_solution.py
@@ -4,23 +4,24 @@ import pytest
 
 class TestSum():
     def test_sum(self):
-        assert SumSolution().compute(1, 2) == 3
-        assert SumSolution().compute(0, 0) == 0  # Minimum valid values
-        assert SumSolution().compute(100, 100) == 200  # Maximum valid values
-        assert SumSolution().compute(0, 100) == 100  # Boundary case
-        assert SumSolution().compute(50, 50) == 100  # Regular case
+        assert SumSolution().sum(1, 2) == 3
+        assert SumSolution().sum(0, 0) == 0  # Minimum valid values
+        assert SumSolution().sum(100, 100) == 200  # Maximum valid values
+        assert SumSolution().sum(0, 100) == 100  # Boundary case
+        assert SumSolution().sum(50, 50) == 100  # Regular case
 
     def test_sum_invalid_inputs(self):
             """Test for invalid inputs"""
             with pytest.raises(ValueError) as excinfo:
-                SumSolution().compute(150, 20)
+                SumSolution().sum(150, 20)
             assert str(excinfo.value) == "x must be between 0 and 100, got 150"
 
             with pytest.raises(ValueError) as excinfo:
-                SumSolution().compute(20, -1)
+                SumSolution().sum(20, -1)
             assert str(excinfo.value) == "y must be between 0 and 100, got -1"
 
             with pytest.raises(ValueError) as excinfo:
-                SumSolution().compute(-1, -1)
+                SumSolution().sum(-1, -1)
             assert str(excinfo.value) == "x must be between 0 and 100, got -1"
+
 
