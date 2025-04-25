dabb3e6f4a454137eed91a6716975809bf55a847
sbx_user1051 <sbx_user1051@169.254.4.197>
2025-04-25 14:02:25 +0000

    Fri Apr 25 14:02:25 UTC 2025

--- lib/solutions/HLO/hello_solution.py
+++ lib/solutions/HLO/hello_solution.py
@@ -3,7 +3,5 @@ class HelloSolution:
     
     # friend_name = unicode string
     def hello(self, friend_name = None):
-        if friend_name is None or friend_name == "":
-            return "Hello, World!"
-        return f"Hello, {str(friend_name)}!"
+        return "Hello, World!"
 
--- test/solution_tests/HLO_R1/test_hello_solution.py
+++ test/solution_tests/HLO_R1/test_hello_solution.py
@@ -2,10 +2,10 @@ from lib.solutions.HLO.hello_solution import HelloSolution
 
 class TestHello:
     def test_hello_valid_inputs(self):
-        assert HelloSolution().hello("Alice") == "Hello, Alice!"
-        assert HelloSolution().hello("Bobby") == "Hello, Bobby!"
+        assert HelloSolution().hello("Alice") == "Hello, World!"
+        assert HelloSolution().hello("Bobby") == "Hello, World!"
         assert HelloSolution().hello("World") == "Hello, World!"
-        assert HelloSolution().hello("世界") == "Hello, 世界!"
+        assert HelloSolution().hello("世界") == "Hello, World!"
     
     def test_hello_valid_empty_input(self):
         assert HelloSolution().hello("") == "Hello, World!"
@@ -13,5 +13,5 @@ class TestHello:
         assert HelloSolution().hello() == "Hello, World!"
 
     def test_hello_non_string(self):
-        assert HelloSolution().hello(123) == "Hello, 123!"
-        assert HelloSolution().hello(["a", "b"]) == "Hello, ['a', 'b']!"
+        assert HelloSolution().hello(123) == "Hello, World!"
+        assert HelloSolution().hello(["a", "b"]) == "Hello, World!"
