deb1fff94d5aa90f3c5cb4483447b7efe91b4bee
sbx_user1051 <sbx_user1051@169.254.4.197>
2025-04-25 15:02:37 +0000

    Fri Apr 25 15:02:37 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,6 +1,43 @@
+from collections import Counter
 
 class CheckoutSolution:
 
+    def __init__ (self):
+
+        # Prices and special offers as class attributes
+        self.items = [
+            {'item': 'A', 'price': 50, 'offer': (3, 130)},
+            {'item': 'B', 'price': 30, 'offer': (2, 45)},
+            {'item': 'C', 'price': 20, 'offer': None},
+            {'item': 'D', 'price': 15, 'offer': None},
+        ]
+        
+    def _get_item_lookup(self) -> dict:
+        return {item['item']: item for item in self.items}
+
+    def _is_valid_input (self, skus: str, item_lookup: dict) -> bool:
+        return isinstance(skus, str) and all(c in item_lookup for c in skus)
+    
+    def _apply_offer(self, item_details: dict, count:int) -> int:
+        price = item_details['price']
+        offer = item_details['offer']
+        if offer:
+            offer_qty, offer_price = offer
+            return (count // offer_qty) * offer_price + (count % offer_qty) * price
+        return count * price
+
     # skus = unicode string
-    def checkout(self, skus):
-        raise NotImplementedError()
+    def checkout(self, skus: str) -> int:
+        item_lookup = self._get_item_lookup()
+        if not self._is_valid_input(skus, item_lookup):
+            return -1
+        
+        total =  0 
+        counts = Counter(skus)
+
+        for item, count in counts.items():
+            item_details = item_lookup[item]
+            total += self._apply_offer(item_details, count)
+
+        return total
+
