0782fb3d0f26a04affc9536ff45be9b7fe7a4004
sbx_user1051 <sbx_user1051@169.254.4.197>
2025-04-25 12:19:29 +0000

    Fri Apr 25 12:19:29 UTC 2025

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -1,7 +1,7 @@
 
 class SumSolution:
      
-    def compute(self, x, y): # Main method to compute the sum of x and y.
+    def sum(self, x, y): # Main method to compute the sum of x and y.
         self._validate_input(x, 'x')
         self._validate_input(y, 'y')
         return x + y
@@ -9,4 +9,5 @@ class SumSolution:
     def _validate_input (self, value: int, name: str): # Internal method to valdate range (0 - 100)
         if not (0 <= value <= 100):
             raise ValueError(f"{name} must be between 0 and 100, got {value}")
+
 
