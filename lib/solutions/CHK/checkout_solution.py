5f4f94b7ebe63b82ee66334c4b3b613d2fcbeea6
sbx_user1051 <sbx_user1051@169.254.4.197>
2025-04-25 18:12:40 +0000

    Fri Apr 25 18:12:40 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -97,12 +97,15 @@ class CheckoutSolution:
                 # If no offer, just add the regular price * count
                 total += item_details['price'] * count
 
+        print (item, free_count)
+
         # Final bill adjustment — subtract free item prices from total
         for item, free_count in self.free_items_given.items():
-            print (item, free_count)
+            
             if item in counts:  # Only subtract if the item exists in basket
                 actual_free = min(free_count, counts[item])  # Only discount items that exist
                 total -= actual_free * item_lookup[item]['price']
 
         return total
+
 
