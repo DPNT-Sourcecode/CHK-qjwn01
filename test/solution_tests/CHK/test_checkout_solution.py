600c3f9f62c3f093a0ac51f58ca00841debed088
sbx_user1051 <sbx_user1051@169.254.4.197>
2025-04-25 20:20:24 +0000

    Fri Apr 25 20:20:24 UTC 2025

--- test/solution_tests/CHK/test_checkout_solution.py
+++ test/solution_tests/CHK/test_checkout_solution.py
@@ -6,8 +6,10 @@ class TestCheckoutSolution:
     def test_checkout_valid_skus_with_offer(self):
         # Test that the correct offers are applied
         assert CheckoutSolution().checkout("AAABBCD") == 210  # 3A for 130, 2B for 45, 1C for 20, 1D for 15
-        assert CheckoutSolution().checkout("AAA") == 200  # 3A for 130
-        assert CheckoutSolution().checkout("AAAAA") == 130  # 3A for 130
+        assert CheckoutSolution().checkout("AAA") == 130  # 3A for 130
+        assert CheckoutSolution().checkout("AAAAA") == 200  # 5A for 200
+        assert CheckoutSolution().checkout("AAAAAA") == 250  # 5A for 200 + A for 50
+        assert CheckoutSolution().checkout("AAAAAAA") == 300  # 5A for 200 + 2A for 100
         assert CheckoutSolution().checkout("BB") == 45  # 2B for 45
         assert CheckoutSolution().checkout("CC") == 40  # 2C for regular price
         assert CheckoutSolution().checkout("D") == 15  # Single D at regular price
@@ -50,4 +52,5 @@ class TestCheckoutSolution:
     #def test_checkout_special_offer_with_b(self):
         # Test that 2 E's with 1 B in the basket correctly applies the offer and sets B to 0 cost
         #assert CheckoutSolution().checkout("EEB") == 80  # 2E for 80, 1 B free (B's cost should be 0)
+
 
